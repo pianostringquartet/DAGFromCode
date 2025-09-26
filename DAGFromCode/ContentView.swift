8baa27c3e0179cdc09df398c7860bee9641a63fd
Chris Clampitt <christian.clampitt@nyu.edu>
2025-09-25 22:04:27 -0600

    prefer graph view as defaukt

--- DAGFromCode/ContentView.swift
+++ DAGFromCode/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = 0
+    @State private var selectedTab = 1
 
     var body: some View {
         TabView(selection: $selectedTab) {
